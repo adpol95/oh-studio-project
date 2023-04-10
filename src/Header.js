787547d297fab9532aedab6816d2a41e4958a5ac
adpol95 <adpol95@gmail.com>
2023-04-10 19:54:16 +0300

    Added infrastructure for header active change style background

--- src/Header.js
+++ src/Header.js
@@ -1,16 +1,40 @@
-import { Link } from "react-router-dom";
-import './header.css';
+import {Link} from "react-router-dom";
+import './App.css';
+import {useState} from "react";
+
 function Header() {
+  const headerList = [
+    {
+      id: 1 + '',
+      value: 'Home',
+      link: '',
+      active: true
+    },
+    {
+      id: 2 + '',
+      value: 'Profile',
+      link: 'profile',
+      active: false
+    },
+    {
+      id: 3 + '',
+      value: 'Contact',
+      link: 'contact',
+      active: false
+    }
+  ]
+
+  const [activeHead, setActiveHead] = useState(headerList);
+  const selectHeadSec = (id) => {
+    // setActiveHead(activeHead.map(el => { ...el, active: id === el.id } ));
+    console.log(activeHead)
+  }
+
+
   return (
     <header className={'header'}>
-      <div className='leftSec'>
-        <Link to={'/'}> Home </Link>
-      </div>
-      <div className='midlSec'>
-        <Link to={'profile'}> Profile </Link>
-      </div>
-      <div className='righSec'>
-        <Link to={'contact'}> Contact </Link>
+      <div className={'mainSec'}>
+        {activeHead.map(el => <div onClick={() => selectHeadSec(el.id)} id={el.id} style={{background: el.active ? "white" : ""}}><Link to={'/' + el.link} className={'links'}> {el.value} </Link></div>)}
       </div>
     </header>
   )
